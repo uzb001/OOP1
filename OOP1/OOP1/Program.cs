c3ba22256071d41294f79e913296b987d4587533
uzb001 <137428777+uzb001@users.noreply.github.com>
2023-08-16 18:10:00 +0500

    task2

--- OOP1/OOP1/Program.cs
+++ OOP1/OOP1/Program.cs
@@ -4,11 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Rectangle rectangle = new Rectangle();
-            rectangle.Width = 10;
-            rectangle.Height = 4;
-            rectangle.DisplayInfo();
+            Student student = new Student();
+            student.Name = "Oybek";
+            student.RollNumber = "TM222-003";
+            student.Marks = new int[] {1,2,3,4,5,6,7,1};
+            student.DisplayInfo();
         }
     }
-   
+    class Student
+    {
+        public string Name { get; set; }
+        public string RollNumber { get; set; }
+        public int[] Marks { get; set; }
+        public double CalculateAvarage()
+        {
+            double sum = 0, counter = 0;
+            for(int i = 0; i < Marks.Length; i++)
+            {
+                sum+= Marks[i];
+                counter++;
+            }
+            return sum / counter;
+        }
+
+        public void DisplayInfo() 
+        {
+            Console.WriteLine($"Name: {Name}, RollNumber: {RollNumber}, AvarageMarks: {CalculateAvarage()}");
+        }
+
+    }
 }
