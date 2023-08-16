ef969b67e9524e544d2da26fdaf69889a32a1c38
uzb001 <137428777+uzb001@users.noreply.github.com>
2023-08-16 15:57:19 +0500

    task1

---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 10;
+            rectangle.Height = 4;
+            rectangle.DisplayInfo();
+        }
+    }
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double CalculadeArea ()
+        {
+            return Width * Height;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Area of Rectangular: {CalculadeArea()}");
+        }
+    }
+}
